9304e3f22f7451a071698c22f40d46fdab31415b
eddypuji <edipuji07@gmail.com>
2021-05-03 18:12:59 +0700

    Mengerjakan UTS PBO IV

---
+++ storage/framework/views/7c4fa0a34e89065a393b993c76c5ed039c5b6cf2.php
@@ -0,0 +1,11 @@
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Yamaap namanya juga orang dagang
+    </div>
+    <!-- Default to the left -->
+    <center><strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io"><b>KangDisen</b> Studio</a>.</strong> All rights reserved.
+  </footer>
+<?php /**PATH C:\xampp\htdocs\uts_eddypujiallo\resources\views/layout/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d966e1fcfbeb4a83b8e151fb399b7340c0ca7e6c.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>KangDisen Studio</title>
+
+  <!-- Bootstrap -->
+    <link href="<?php echo e(asset ('')); ?>css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset ('')); ?>css/custom.css">
+
+    
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo e(asset ('')); ?>plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?php echo e(asset ('')); ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo e(asset ('')); ?>dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="<?php echo e(asset ('')); ?>"><b>KangDisen</b>Studio</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Login dulu kasep :')</p>
+
+      <form action="<?php echo e(asset ('')); ?>masuk" method="post">
+      <?php echo e(csrf_field()); ?>
+
+        <div class="input-group mb-3">
+          <input name="txtuser" type="text" class="form-control" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input name="txtpass" type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Ingat aku
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-info btn-block">Masuk</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <div class="social-auth-links text-center mb-3">
+        <p>- ATAU -</p>
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i> Masuk dengan Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i> Masuk dengan Google+
+        </a>
+      </div>
+      <!-- /.social-auth-links -->
+
+      <p class="mb-1">
+        <a href="forgot-password.html">Lupa Sandi</a>
+      </p>
+      <p class="mb-0">
+        <a href="register.html" class="text-center">Daftar Akun Baru</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?php echo e(asset ('')); ?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo e(asset ('')); ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo e(asset ('')); ?>dist/js/adminlte.min.js"></script>
+
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\uts_eddypujiallo\resources\views/auth/login.blade.php ENDPATH**/ ?>
